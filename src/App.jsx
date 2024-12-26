b81fffe9c63ea084cb2c766c975fcd4b21060109
Aatmik-06 <adi120kush@gmail.com>
2024-12-26 20:25:28 +0530

    ui updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-import { useState } from 'react'
 import './index.css'
 import axios from 'axios'
 import './App.css'
 import ReactMarkdown from 'react-markdown';
+
+import React, { useState, useEffect, useRef } from "react";
+import { _vantaEffect } from 'vanta/dist/vanta.clouds.min';
+import CLOUDS from "vanta/dist/vanta.clouds.min"
+import * as THREE from "three";
+import { Container } from 'react-bootstrap';
+
 function App() {
+
+
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        CLOUDS({
+          el: vantaRef.current,
+          THREE: THREE,
+          mouseControls: false,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200,
+          minWidth: 200,
+          scale: 1.0,
+          scaleMobile: 1.0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
+
+
+
+
+
   const [question, setQuestion] = useState("");
   const [answer, setAnswer]= useState("");
   async function generatedanswer() {
@@ -29,21 +66,35 @@ function App() {
   }
   return (
     <>
+    <Container  fluid  >
+      <div ref={vantaRef} style={{ height:"100%", width: "100%" }}>
      <h1>AI Chat Bot</h1>
      <div id='ans'>
      <div  className="flex-1 overflow-y-auto mb-4 rounded-lg bg-white shadow-lg p-4 hide-scrollbar" >
     
-   
-      
-      <ReactMarkdown id="ans" className="overflow-auto hide-scrollbar items-center">
+      <ReactMarkdown id="ans" className="overflow-auto hide-scrollbar items-center" >
       {answer}
       </ReactMarkdown>
+     
      </div>
      </div>
      <textarea value={question} id='input' onChange={(e) => setQuestion(e.target.value)} placeholder="Ask anything..." ></textarea> <br /><br />
-     <button onClick={ generatedanswer} >Generate answer</button>
+    
+     <div >
+     <button onClick={ generatedanswer}>Generate answer</button>
+     
+     </div>
+     </div>
+      </Container>
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
