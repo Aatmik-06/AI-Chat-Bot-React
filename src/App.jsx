d3e8920b6b3f22fd7e0efdede213e5b3778cf691
Aatmik-06 <adi120kush@gmail.com>
2024-12-28 21:34:23 +0530

    ui uodated

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './index.css'
 import axios from 'axios'
 import './App.css'
 import ReactMarkdown from 'react-markdown';
-
+import Spinner from 'react-bootstrap/Spinner';
 import React, { useState, useEffect, useRef } from "react";
 import Waves from "vanta/dist/vanta.waves.min"
-import * as THREE from "three";
 import { Container } from 'react-bootstrap';
-
+import { TailSpin } from "react-loader-spinner";
+import Loader from './Loader';
+import { LoaderUtils } from 'three';
 const App = () => {
 
   // Created background effect
@@ -22,6 +23,7 @@ const App = () => {
           touchControls: true,
           gyroControls: false,
           minHeight: 200.00,
+          color: "#4f3390",
         })
       );
     }
@@ -30,17 +32,21 @@ const App = () => {
     };
   }, [vantaEffect]);
 
-
+ 
 
   // Created state for question and answer
   const [question, setQuestion] = useState("");
   const [displayquestion, setDisplayquestion] = useState("");
   const [answer, setAnswer]= useState("");
 
+ 
+
   // Created function to generate answer
+  
   async function generatedanswer() {
-    setAnswer("Loading...")
+    setAnswer("Thinking...")
     try {
+      
       const response = await axios({
         url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAmI8sN1T5SzP3AlM-5CPxFRlxFUyxJvd0",
         method:"post",
@@ -52,34 +58,50 @@ const App = () => {
       setAnswer(response['data']['candidates'][0]['content']['parts'][0]['text']);
       setQuestion("")
       setDisplayquestion(question)
-      if(setQuestion==""){
-      setAnswer("")
-    }
+      
+     
     document.getElementById('question').style.display="block"
-    } catch (error) {
+    
+    } 
+     catch (error) {
       console.log(error);
       setAnswer("Sorry - Something went wrong. Please try again!");
     }
+  }
+  
+  if(answer==""){
+    setAnswer("Hey there! I'm Genie AI Chatbot. How can I help you today?")
   }
   return (
     <>
     <Container fluid  ref={vantaRef} id='cont'>
     <div id='main'>
      <h1>Genie AI Chatbot</h1>
-    
+     
      <div id="ans"  className="flex-1 overflow-y-auto mb-4 rounded-lg shadow-lg p-4 hide-scrollbar" >
       <h3 style={{display:"none"}} id='question'> Question: {displayquestion} </h3>
-      <ReactMarkdown  className="overflow-auto hide-scrollbar items-center" >
-          
+      
+      <ReactMarkdown  className="overflow-auto hide-scrollbar items-center" id='mkd' >   
+      
       {answer}
+      
       </ReactMarkdown>
+     
+     
      </div>
      
      <textarea value={question} id='input'  onChange={(e) => setQuestion(e.target.value)} placeholder="Ask anything..." ></textarea> <br /><br />
     
      <div>
-     <button eventKey="1" onClick={generatedanswer}>Generate answer</button>
      
+     <button class="btn" onClick={generatedanswer}> 
+     <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" class="sparkle">
+        <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
+    </svg>
+    <span class="text">Generate</span>
+  
+    </button>
+    
      </div>
      </div>
     
